431393713d74ce9c5af28a87e94f7ef7bb025957
saikumar8639 <75746273+saikumar8639@users.noreply.github.com>
2020-12-13 18:43:13 +0530

    Update dictionary_assignment_day2_letsupgrade.py

--- dictionary_assignment_day2_letsupgrade.py
+++ dictionary_assignment_day2_letsupgrade.py
@@ -1,5 +1,3 @@
-Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> diction={'sai':48,'sampath':49,'naveen':47,'shiva':48}
 >>> diction
 {'sai': 48, 'sampath': 49, 'naveen': 47, 'shiva': 48}
